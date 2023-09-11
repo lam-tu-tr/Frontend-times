b2656f4048b80949a510b9b76a9cb52afcf722f2
Lam Tran <101381468+lam-tu-tr@users.noreply.github.com>
2023-09-11 13:19:27 -0700

    Imported svg images to Tech

--- app/components/Tech/Tech.tsx
+++ app/components/Tech/Tech.tsx
@@ -1,6 +1,30 @@
 import React from "react";
 import style from "./Tech.module.scss";
+import Image from "next/image";
+
+import css from "../../assets/icons/css.svg";
+import express from "../../assets/icons/express.svg";
+import figma from "../../assets/icons/figma.svg";
+import html from "../../assets/icons/html.svg";
+import js from "../../assets/icons/javascript.svg";
+import next from "../../assets/icons/nextjs.svg";
+import node from "../../assets/icons/nodejs.svg";
+import prisma from "../../assets/icons/prisma.svg";
+import ts from "../../assets/icons/typescript.svg";
 
 export default function Tech() {
-  return <div id={style.tech}>Tech</div>;
+  const logos = [html, css, js, ts, next, node, express, prisma];
+  return (
+    <div id={style.tech}>
+      <section>
+        {logos.map((tech, index) => {
+          return (
+            <aside key={index}>
+              <Image src={tech} width={60} height={60} alt={"tech"} />
+            </aside>
+          );
+        })}
+      </section>
+    </div>
+  );
 }
