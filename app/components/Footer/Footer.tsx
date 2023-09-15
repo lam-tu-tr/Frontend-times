9ad796868b8241d1729a34c2889162bf8279d12e
Lam Tran <101381468+lam-tu-tr@users.noreply.github.com>
2023-09-15 12:30:42 -0700

    Converted footer grid to flex, added advertisement area

--- app/components/Footer/Footer.tsx
+++ app/components/Footer/Footer.tsx
@@ -1,14 +1,17 @@
 "use client";
+
+import Image from "next/image";
+
 import { forwardRef } from "react";
 import styles from "./Footer.module.scss";
 
-//icons
 import { FaGithub } from "react-icons/fa";
 import { FaLinkedin } from "react-icons/fa";
-import { FaPhone } from "react-icons/fa6";
-import { HiMail } from "react-icons/hi";
+import { FaSquarePhone } from "react-icons/fa6";
+import { BsMailbox2 } from "react-icons/bs";
 
 interface ChildProps {}
+import pointer from "../../assets/icons/pointing-cursor.svg";
 
 const Footer = forwardRef<HTMLElement, ChildProps>(({}, footerRef) => {
   return (
@@ -18,16 +21,27 @@ const Footer = forwardRef<HTMLElement, ChildProps>(({}, footerRef) => {
         </form> */}
         Contact
       </section>
-      <section>Your Ads Here</section>
+
       <section id={styles.contact}>
-        <span>
-          <HiMail className="w-14 h-14" />
-          Lam.tu.tr@gmail.com
-        </span>
-        <span>
-          <FaPhone className="w-10 h-10" />
-          (669) - 246 2649
-        </span>
+        <aside id={styles.ad}>
+          <Image
+            src={pointer}
+            alt="pointer"
+            className="w-12 scale-x-[-100%] fill-blue-500"
+          />
+          <span>Your Website Here</span>
+          <Image src={pointer} alt="pointer" className="w-12" />
+        </aside>
+        <aside id={styles.info}>
+          <span>
+            <BsMailbox2 className="w-10 h-10" />
+            Lam.tu.tr@gmail.com
+          </span>
+          <span>
+            <FaSquarePhone className="w-10 h-10" />
+            (669) - 246 2649
+          </span>
+        </aside>
       </section>
       <section id={styles.social}>
         <FaGithub className="w-10 h-10" />
