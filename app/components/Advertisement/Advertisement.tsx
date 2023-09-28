134634cdcad5b9118b33dc12bf12eefebdf97d88
Lam Tran <101381468+lam-tu-tr@users.noreply.github.com>
2023-09-27 21:48:11 -0700

    Fully functional website

--- app/components/Advertisement/Advertisement.tsx
+++ app/components/Advertisement/Advertisement.tsx
@@ -1,20 +1,23 @@
 import React from "react";
 import style from "./Advertisement.module.scss";
 import Image from "next/image";
-import pointer from "../../assets/icons/pointing-cursor.svg";
+
+import pointer_cursor from "@/app/assets/icons/pointing-cursor.svg";
+
+import Pointer_cursor from "@/app/assets/react-svg/Pointer";
 
 export default function Advertisement() {
   return (
     <div id={style.ad}>
       Advertisement
       <aside className="padded_container">
-        <span>
-          {/* inverted using tailwind */}
-          <Image src={pointer} alt="pointer" className="scale-x-[-100%] " />
+        {/* inverted using tailwind */}
+        <span className="transform -scale-x-100">
+          <Pointer_cursor className="svg_fill" />
         </span>
         <h3>Your Website Here</h3>
         <span>
-          <Image src={pointer} alt="pointer" />
+          <Pointer_cursor className="svg_fill" />
         </span>
       </aside>
     </div>
--- app/components/Tech/Tech.tsx
+++ app/components/Tech/Tech.tsx
@@ -2,19 +2,42 @@ import React from "react";
 import style from "./Tech.module.scss";
 import Image from "next/image";
 
-import css from "../../assets/icons/css.svg";
-import express from "../../assets/icons/express.svg";
-import figma from "../../assets/icons/figma.svg";
-import html from "../../assets/icons/html.svg";
-import js from "../../assets/icons/javascript.svg";
-import next from "../../assets/icons/nextjs.svg";
-import node from "../../assets/icons/nodejs.svg";
-import prisma from "../../assets/icons/prisma.svg";
-import ts from "../../assets/icons/typescript.svg";
-import sass from "../../assets/icons/sass.svg";
+// import css from "../../assets/icons/css.svg";
+// import express from "../../assets/icons/express.svg";
+// import figma from "../../assets/icons/figma.svg";
+// import html from "../../assets/icons/html.svg";
+// import js from "../../assets/icons/javascript.svg";
+// import next from "../../assets/icons/nextjs.svg";
+// import node from "../../assets/icons/nodejs.svg";
+// import prisma from "../../assets/icons/prisma.svg";
+// import ts from "../../assets/icons/typescript.svg";
+// import sass from "../../assets/icons/sass.svg";
+import Css from "@/app/assets/react-svg/tech/Css";
+import Express from "@/app/assets/react-svg/tech/Express";
+import Figma from "@/app/assets/react-svg/tech/Figma";
+import Html from "@/app/assets/react-svg/tech/Html";
+import Js from "@/app/assets/react-svg/tech/Js";
+import Next from "@/app/assets/react-svg/tech/Next";
+import Node from "@/app/assets/react-svg/tech/Node";
+import Prisma from "@/app/assets/react-svg/tech/Prisma";
+import Sass from "@/app/assets/react-svg/tech/Sass";
+import Ts from "@/app/assets/react-svg/tech/Ts";
 
 export default function Tech() {
-  const logos = [html, css, sass, js, ts, next, node, express, prisma, figma];
+  // const logos = [html, css, sass, js, ts, next, node, express, prisma, figma];
+
+  const logos = [
+    <Html key="html" />,
+    <Css key="css" />,
+    <Sass key="sass" />,
+    <Js key="js" />,
+    <Ts key="ts" />,
+    <Express key="express" className="svg_fill" />,
+    <Next key="next" className="svg_fill" />,
+    <Node key="node" />,
+    <Prisma key="prisma" className="svg_fill" />,
+    <Figma key="figma" />,
+  ];
   return (
     <>
       <div id={style.heading_container}>
@@ -25,21 +48,21 @@ export default function Tech() {
           {logos.map((tech, index) => {
             return (
               <li key={index} className="icon">
-                <Image src={tech} alt={"tech"} />
+                {tech}
               </li>
             );
           })}
           {logos.map((tech, index) => {
             return (
               <li key={index} className="icon">
-                <Image src={tech} alt={"tech"} />
+                {tech}
               </li>
             );
           })}
           {logos.map((tech, index) => {
             return (
               <li key={index} className="icon">
-                <Image src={tech} alt={"tech"} />
+                {tech}
               </li>
             );
           })}
