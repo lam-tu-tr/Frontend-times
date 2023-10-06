e8bc2ba6358a275cccb1bb009203e735d1b8cce1
Lam Tran <101381468+lam-tu-tr@users.noreply.github.com>
2023-10-05 18:47:37 -0700

    Added favicon and edited SEO metadata

---
+++ app/icon.tsx
@@ -0,0 +1,28 @@
+import { ImageResponse } from "next/server";
+import { BiCodeAlt } from "react-icons/bi";
+
+// Route segment config
+export const runtime = "edge";
+
+// Image metadata
+export const size = {
+  width: 14,
+  height: 14,
+};
+export const contentType = "image/png";
+
+// Image generation
+export default function Icon() {
+  return new ImageResponse(
+    (
+      // ImageResponse JSX element
+      <BiCodeAlt />
+    ),
+    // ImageResponse options
+    {
+      // For convenience, we can re-use the exported icons size metadata
+      // config to also set the ImageResponse's width and height.
+      ...size,
+    }
+  );
+}
